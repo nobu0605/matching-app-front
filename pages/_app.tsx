42ab94a50457069b844342e988f668035423f3d5
nobu0605 <nobublack555@gmail.com>
2021-11-27 23:41:43 +0900

    feature/apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,22 @@ import "semantic-ui-css/semantic.min.css"
 import { RecoilRoot } from "recoil"
 import type { AppProps } from "next/app"
 import { AuthRouteWrapper } from "../components/layout/AuthRouteWrapper"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_API_BASE_URL + "/graphql",
+  cache: new InMemoryCache(),
+})
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <AuthRouteWrapper>
-        <Component {...pageProps} />
-      </AuthRouteWrapper>
-    </RecoilRoot>
+    <ApolloProvider client={client}>
+      <RecoilRoot>
+        <AuthRouteWrapper>
+          <Component {...pageProps} />
+        </AuthRouteWrapper>
+      </RecoilRoot>
+    </ApolloProvider>
   )
 }
 
