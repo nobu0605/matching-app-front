3122b677560c0d319c09d78e8bf49a3f4111d81c
nobu0605 <nobublack555@gmail.com>
2021-11-27 19:12:32 +0900

    feature/register

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import React from "react"
+import "../styles/globals.css"
+import "semantic-ui-css/semantic.min.css"
+import { RecoilRoot } from "recoil"
+import type { AppProps } from "next/app"
+import { AuthRouteWrapper } from "../components/layout/AuthRouteWrapper"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <AuthRouteWrapper>
+        <Component {...pageProps} />
+      </AuthRouteWrapper>
+    </RecoilRoot>
+  )
 }
 
-export default MyApp
+export default App
